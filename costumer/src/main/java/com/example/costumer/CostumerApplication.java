40d5a952b03138d8c78e85146ef1c8aad5f58e32
daniel12142007 <danijelahatzanov@gmail.com>
2024-10-11 18:16:45 +0600

    I add eureka

--- costumer/src/main/java/com/example/costumer/CostumerApplication.java
+++ costumer/src/main/java/com/example/costumer/CostumerApplication.java
@@ -2,8 +2,10 @@ package com.example.costumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class CostumerApplication {
 
     public static void main(String[] args) {
